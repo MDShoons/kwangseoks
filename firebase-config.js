ce7e03c28d02f75adb69bd314f3f55337d521647
MDShoons <kos20050627@gmail.com>
2026-05-24 01:18:24 +0900

    Update firebase-config.js formatting

--- firebase-config.js
+++ firebase-config.js
@@ -10,7 +10,6 @@ const firebaseConfig = {
   appId: "1:322477795788:web:9f6a9c2c8d26c1a76d5569",
   measurementId: "G-9RG0YXCMY9"
 };
-
 // 관리자 이메일 목록
 // 비밀번호는 절대 이 파일에 저장하지 마세요.
 // 아래 두 계정은 Firebase Authentication에서 직접 생성하거나, 사이트 회원가입으로 생성해야 합니다.
